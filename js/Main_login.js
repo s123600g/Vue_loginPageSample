e66781547a1a210b271ed50b0c86edca78850090
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2020-10-16 00:05:25 +0800

    更新專案

--- js/Main_login.js
+++ js/Main_login.js
@@ -1,8 +1,17 @@
 var data_list = {
   email: "test@login.com",
+  pwd: "123",
 };
 
-var login_form = new Vue({
-  el: "#login",
-  data: data_list,
+// 監聽確認DOM都載入完成所有物件，在進行Vue元件配置
+document.addEventListener("DOMContentLoaded", function (event) {
+  var login_form = new Vue({
+    el: "#MainArea",
+    data: data_list,
+    methods: {
+      sendLogin: function (event) {
+        alert(`email: ${this.email} , pwd: ${this.pwd}`);
+      },
+    },
+  });
 });
